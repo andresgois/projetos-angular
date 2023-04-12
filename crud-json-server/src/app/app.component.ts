943c903f9414ebab6479463ddca9214df8265747
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-04-11 21:06:17 -0300

    :sparkles:	feat: listagems dos componentes

--- crud-json-server/src/app/app.component.ts
+++ crud-json-server/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioServiceService } from './services/usuario-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-json-server';
+
+  constructor(private service: UsuarioServiceService){}
 }
--- crud-json-server/src/app/app.module.ts
+++ crud-json-server/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { appRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule,
-    appRoutingModule
+    appRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
