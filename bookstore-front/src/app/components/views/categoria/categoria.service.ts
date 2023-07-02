d326e18c4ee9dde3d5644f9ca9cfa6895b68bddf
André Gois <andresousalee@gmail.com>
2023-07-02 19:41:34 -0300

    :sparkles: feat: ajustando servico de delecao de categorias

--- bookstore-front/src/app/components/views/categoria/categoria.service.ts
+++ bookstore-front/src/app/components/views/categoria/categoria.service.ts
@@ -19,6 +19,16 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(url);
   }
 
+  findById(id: String):Observable<Categoria>{
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.get<Categoria>(url);
+  }
+
+  deletar(id: String):Observable<void>{
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.delete<void>(url);
+  }
+
   create(categoria: Categoria): Observable<Categoria>{
     const url = `${this.baseUrl}/categorias`;
     return this.http.post<Categoria>(url, categoria);
--- bookstore-front/src/app/components/views/categorias/categoria-delete/categoria-delete.component.ts
+++ bookstore-front/src/app/components/views/categorias/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../../categoria/categoria.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from '../categoria.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categoria-delete',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor() { }
+    categoria: Categoria = {
+        id: '',
+        nome: '',
+        descricao: ''
+    }
+
+  constructor(
+    private service: CategoriaService,
+    private urlPagina: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.categoria.id = this.urlPagina.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe( r => {
+        console.log(r);
+        this.categoria = r;
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(["categorias"]);
+  }
+
+  deletar(): void {
+    this.service.deletar(this.categoria.id!).subscribe( r => {
+        this.router.navigate(['categorias']);
+        this.service.mensagem('Categoria deletada com sucesso!');
+    }, e => {
+            this.service.mensagem(e.error.error)
+    });
   }
 
 }
